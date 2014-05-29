5e35f4960e2806c251df89eab8a56ffda6fb609e
grepwood <grepwood@sucs.org>
2014-05-29 02:02:28 +0100

    first batch of offsets

---
+++ offsets.h
@@ -0,0 +1,14 @@
+#ifdef CIPSOFT
+#	if TIBIA == 870
+	uint32_t offset[10] = {	0x085acde0,
+				0x085ace50
+				0x085acec0
+				0x085acf30
+				0x085acfa0
+				0x085ad010
+				0x085ad080
+				0x085ad0f0
+				0x085ad160
+				0x085ad1d0};
+#	endif /*TIBIA*/
+#endif /*CIPSOFT*/
